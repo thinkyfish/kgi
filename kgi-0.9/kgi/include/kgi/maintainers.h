7fb77d3350a4bee72908b130e76b5cb4663b4ec1
seeger_s <seeger_s>
2000-04-26 15:08:29 +0000

    - email addresses fixed to kgi-develop@sourceforge

--- kgi-0.9/kgi/include/kgi/maintainers.h
+++ kgi-0.9/kgi/include/kgi/maintainers.h
@@ -7,12 +7,11 @@
 #define	_kgi_maintainers_h
 
 #define	The_GGI_Project	"The GGI Project <ggi-develop@eskimo.com>"
-#define	Emmanuel_Marty	"Emmanuel Marty <core@mirius.fr>"
-#define	Jon_Taylor	"Jon M. Taylor <ggi-kgi@kludge.org>"
-#define	Jonas_Borgstrom	"Jonas Borgstroem"
-#define	Steffen_Seeger	"Steffen Seeger <ggi-kgi@kludge.org>"
-#define	Brian_S_Julin	"Brian S. Julin <bri@calyx.com>"
-#define	Jos_Hulzink	"Jos Hulzink <ggi-kgi@kludge.org>"
-#define	Johan_Karlberg	"Johan Karlberg <ggi-kgi@kludge.org>"
+#define	Emmanuel_Marty	"Emmanuel Marty <kgi-develop@lists.sourceforge.net>"
+#define	Jon_Taylor	"Jon M. Taylor <kgi-develop@lists.sourceforge.net>"
+#define	Steffen_Seeger	"Steffen Seeger <kgi-develop@lists.sourceforge.net>"
+#define	Brian_S_Julin	"Brian S. Julin <kgi-develop@lists.sourceforge.net>"
+#define	Jos_Hulzink	"Jos Hulzink <kgi-develop@lists.sourceforge.net>"
+#define	Johan_Karlberg	"Johan Karlberg <kgi-develop@lists.sourceforge.net>"
 
 #endif	/* _kgi_maintainers_h */
