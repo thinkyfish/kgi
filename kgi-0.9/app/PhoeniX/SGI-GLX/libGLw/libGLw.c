1068d9227abc32da3e44442ff0f1e4e6b44a5b21
seeger_s <seeger_s>
2001-04-02 06:48:56 +0000

    - first import of libGLw

---
+++ kgi-0.9/app/PhoeniX/SGI-GLX/libGLw/libGLw.c
@@ -0,0 +1,8 @@
+/* ----------------------------------------------------------------------------
+**	library stub file
+** ----------------------------------------------------------------------------
+*/
+
+const char * __glw_vendor	= "The KGI Project";
+const char * __glw_revision	= "$Revision:";
+const char * __glw_version	= "ogl-sample-2000-08-07";
