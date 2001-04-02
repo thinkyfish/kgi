9507fb5cffd9a6d3bb9315fa8aedc4eadb4de9a1
seeger_s <seeger_s>
2001-04-02 06:45:55 +0000

    - first import of libGLU

---
+++ kgi-0.9/app/PhoeniX/SGI-GLX/libGLU/libGLU.cc
@@ -0,0 +1,8 @@
+/* ----------------------------------------------------------------------------
+**	library stub file
+** ----------------------------------------------------------------------------
+*/
+
+const char * __glu_vendor	= "The KGI Project";
+const char * __glu_revision	= "$Revision:";
+const char * __glu_version	= "ogl-sample-2000-08-07";
