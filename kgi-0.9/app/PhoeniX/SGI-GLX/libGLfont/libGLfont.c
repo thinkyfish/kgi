74b9d1f2b03f6bd8f195badc59114b412f266a6d
seeger_s <seeger_s>
2001-04-02 06:49:35 +0000

    - first version of libGLfont

---
+++ kgi-0.9/app/PhoeniX/SGI-GLX/libGLfont/libGLfont.c
@@ -0,0 +1,8 @@
+/* ----------------------------------------------------------------------------
+**	library stub file
+** ----------------------------------------------------------------------------
+*/
+
+const char * __glfont_vendor	= "The KGI Project";
+const char * __glfont_revision	= "$Revision:";
+const char * __glfont_version	= "ogl-sample-2000-08-07";
