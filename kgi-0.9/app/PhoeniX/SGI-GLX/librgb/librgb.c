9efe26b73ddfa24aa778e5dce4c6a20ece729535
seeger_s <seeger_s>
2001-04-02 06:29:37 +0000

    - added librgb

---
+++ kgi-0.9/app/PhoeniX/SGI-GLX/librgb/librgb.c
@@ -0,0 +1,8 @@
+/* ----------------------------------------------------------------------------
+**	library stub file
+** ----------------------------------------------------------------------------
+*/
+
+const char * __rgb_vendor	= "The KGI Project";
+const char * __rgb_revision	= "$Revision:";
+const char * __rgb_version	= "ogl-sample-2000-08-07";
