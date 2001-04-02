fe83c3888efe6bc2017695c315c30fde4e447679
seeger_s <seeger_s>
2001-04-02 06:47:43 +0000

    - first import of libglut

---
+++ kgi-0.9/app/PhoeniX/SGI-GLX/libglut/libglut.c
@@ -0,0 +1,8 @@
+/* ----------------------------------------------------------------------------
+**	library stub file
+** ----------------------------------------------------------------------------
+*/
+
+const char * __glut_vendor	= "The KGI Project";
+const char * __glut_revision	= "$Revision:";
+const char * __glut_version	= "ogl-sample-2000-08-07";
