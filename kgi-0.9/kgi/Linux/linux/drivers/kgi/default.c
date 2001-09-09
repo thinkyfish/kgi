8b9e205d77cc250c45c0f18d01b71d0010a17e11
skids <skids>
2001-09-09 23:56:47 +0000

    Take it easy on the Americans

--- kgi-0.9/kgi/Linux/linux/drivers/kgi/default.c
+++ kgi-0.9/kgi/Linux/linux/drivers/kgi/default.c
@@ -13,6 +13,9 @@
 ** ----------------------------------------------------------------------------
 **
 **	$Log: default.c,v $
+**	Revision 1.2  2001/07/03 08:48:41  seeger_s
+**	- added default pointer shape and color lookup table
+**	
 **	Revision 1.1.1.1  2000/04/18 08:50:47  seeger_s
 **	- initial import of pre-SourceForge tree
 **	
@@ -43,6 +46,7 @@ kgi_console_font_t *default_font[CONFIG_KGII_MAX_NR_DEFFONTS] =
 };
 
 #define	CONFIG_KGII_KEYMAP	"default-keymap-de"
+/* #define	CONFIG_KGII_KEYMAP	"default-keymap-us" */
 #include CONFIG_KGII_KEYMAP
 
 #include "default-ptr"
