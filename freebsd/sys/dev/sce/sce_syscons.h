59a9851cac86a77e4701c46cc0066a83f277d89b
bilgey <bilgey>
2009-10-05 05:32:18 +0000

    Clean up/remove unsed code.

--- freebsd/sys/dev/sce/sce_syscons.h
+++ freebsd/sys/dev/sce/sce_syscons.h
@@ -27,8 +27,11 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * From FreeBSD: /head/sys/dev/syscons/syscons.h r189617
+ */
+
+/*
+ * Last synconrized with FreeBSD -r189617:
+ * head/sys/dev/syscons/syscons.h 
  */
 
 #ifndef _DEV_SYSCONS_SYSCONS_H_
@@ -37,121 +40,11 @@
 #include <sys/lock.h>
 #include <sys/mutex.h>
 
-/* machine-dependent part of the header */
-
-#ifdef PC98
-#include <pc98/cbus/sc_machdep.h>
-#elif defined(__i386__)
-/* nothing for the moment */
-#endif
-
-/* default values for configuration options */
-
 #ifndef MAXCONS
-#define MAXCONS		24
+#define MAXCONS	24
 #endif
 
-#ifdef SC_NO_SYSMOUSE
-#undef SC_NO_CUTPASTE
-#define SC_NO_CUTPASTE	1
-#endif
-
-#ifdef SC_NO_MODE_CHANGE
-#undef SC_PIXEL_MODE
-#endif
-
-/* Always load font data if the pixel (raster text) mode is to be used. */
-#ifdef SC_PIXEL_MODE
-#undef SC_NO_FONT_LOADING
-#endif
-
-/* 
- * If font data is not available, the `arrow'-shaped mouse cursor cannot
- * be drawn.  Use the alternative drawing method.
- */
-#ifdef SC_NO_FONT_LOADING
-#undef SC_ALT_MOUSE_IMAGE
-#define SC_ALT_MOUSE_IMAGE 1
-#endif
-
-#ifndef SC_CURSOR_CHAR
-#define SC_CURSOR_CHAR	(0x07)
-#endif
-
-#ifndef SC_MOUSE_CHAR
-#define SC_MOUSE_CHAR	(0xd0)
-#endif
-
-#if SC_MOUSE_CHAR <= SC_CURSOR_CHAR && SC_CURSOR_CHAR < (SC_MOUSE_CHAR + 4)
-#undef SC_CURSOR_CHAR
-#define SC_CURSOR_CHAR	(SC_MOUSE_CHAR + 4)
-#endif
-
-#ifndef SC_DEBUG_LEVEL
-#define SC_DEBUG_LEVEL	0
-#endif
-
-#define DPRINTF(l, p)	if (SC_DEBUG_LEVEL >= (l)) printf p
-
-#ifndef __sparc64__
-#define SC_DRIVER_NAME	"sce"
-#else
-/*
- * Use a different driver name on sparc64 so it does not get confused
- * with the system controller devices which are also termed 'sc' in OFW.
- */
-#define SC_DRIVER_NAME	"sce"
-#endif
-#define SC_VTY(dev)	(((sc_ttysoftc *)tty_softc(tp))->st_index)
-#define SC_DEV(sc, vty)	((sc)->dev[(vty) - (sc)->first_vty])
-#define SC_STAT(tp)	(*((scr_stat **)&((sc_ttysoftc *)tty_softc(tp))->st_stat))
-
-/* printable chars */
-#ifndef PRINTABLE
-#define PRINTABLE(ch)	((ch) > 0x1b || ((ch) > 0x0d && (ch) < 0x1b) \
-			 || (ch) < 0x07)
-#endif
-
-/* macros for "intelligent" screen update */
-#define mark_for_update(scp, x)	{\
-			  	    if ((x) < scp->start) scp->start = (x);\
-				    else if ((x) > scp->end) scp->end = (x);\
-				}
-#define mark_all(scp)		{\
-				    scp->start = 0;\
-				    scp->end = scp->xsize * scp->ysize - 1;\
-				}
-
-/* vty status flags (scp->status) */
-#define UNKNOWN_MODE	0x00010		/* unknown video mode */
-#define SWITCH_WAIT_REL	0x00080		/* waiting for vty release */
-#define SWITCH_WAIT_ACQ	0x00100		/* waiting for vty ack */
-#define BUFFER_SAVED	0x00200		/* vty buffer is saved */
-#define CURSOR_ENABLED 	0x00400		/* text cursor is enabled */
-#define MOUSE_MOVED	0x01000		/* mouse cursor has moved */
-#define MOUSE_CUTTING	0x02000		/* mouse cursor is cutting text */
-#define MOUSE_VISIBLE	0x04000		/* mouse cursor is showing */
-#define GRAPHICS_MODE	0x08000		/* vty is in a graphics mode */
-#define PIXEL_MODE	0x10000		/* vty is in a raster text mode */
-#define SAVER_RUNNING	0x20000		/* screen saver is running */
-#define VR_CURSOR_BLINK	0x40000		/* blinking text cursor */
-#define VR_CURSOR_ON	0x80000		/* text cursor is on */
-#define MOUSE_HIDDEN	0x100000	/* mouse cursor is temporarily hidden */
-
-/* misc defines */
-#define FALSE		0
-#define TRUE		1
-
-/*
-   The following #defines are hard-coded for a maximum text
-   resolution corresponding to a maximum framebuffer
-   resolution of 1600x1200 with an 8x8 font...
-*/
-#define	COL		200
-#define	ROW		150
-
-#define PCBURST		128
-
+#define PCBURST 128
 #define SCEVT_COLD 0
 #define SCEVT_WARM 1
 
@@ -170,7 +63,9 @@ typedef struct sce_ttysoftc {
 extern sce_console *sce_consoles[CONFIG_KGII_MAX_NR_CONSOLES];
 
 extern int sce_mouse_init(void);
-extern int sce_sysmouse_init(void);
 extern void sce_sysmouse_event(kii_event_t *ev);
+extern int sce_sysmouse_init(void);
+extern int sce_sysmouse_ioctl(struct tty *tp, u_long cmd, caddr_t data, 
+		struct thread *td);
 
 #endif /* !_DEV_SYSCONS_SYSCONS_H_ */
