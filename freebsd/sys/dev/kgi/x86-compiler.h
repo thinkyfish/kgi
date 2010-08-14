497c9b1590e779956e0ff7a5d0db0e26125b30ae
bilgey <bilgey>
2010-08-14 13:18:06 +0000

    Style 9'ifer x86-compiler.h abit.

--- freebsd/sys/dev/kgi/x86-compiler.h
+++ freebsd/sys/dev/kgi/x86-compiler.h
@@ -37,21 +37,24 @@
 /*
  * Intel style in/out I/O space.
  */
-extern __inline__ __kgi_u8_t io_in8 (const io_vaddr_t addr) 
+extern __inline__ __kgi_u8_t 
+io_in8(const io_vaddr_t addr) 
 {
 	__kgi_u8_t  val;
 	__asm__ __volatile__ ("inb %w1,%0" : "=a" (val) : "Nd" (addr));
 	return val;
 }
 
-extern __inline__ __kgi_u16_t io_in16(const io_vaddr_t addr)
+extern __inline__ __kgi_u16_t 
+io_in16(const io_vaddr_t addr)
 {
 	__kgi_u16_t val;
 	__asm__ __volatile__ ("inw %w1,%0" : "=a" (val) : "Nd" (addr));
 	return val;
 }
 
-extern __inline__ __kgi_u32_t io_in32(const io_vaddr_t addr)
+extern __inline__ __kgi_u32_t 
+io_in32(const io_vaddr_t addr)
 {
 	__kgi_u32_t val;
 	__asm__ __volatile__ ("inl %w1,%0" : "=a" (val) : "Nd" (addr));
@@ -59,60 +62,72 @@ extern __inline__ __kgi_u32_t io_in32(const io_vaddr_t addr)
 }
 
 
-extern __inline__ void io_out8(const __kgi_u8_t val, const io_vaddr_t addr)
+extern __inline__ void 
+io_out8(const __kgi_u8_t val, const io_vaddr_t addr)
 {
+
 	__asm__ __volatile__ ("outb %b0,%w1" : : "a" (val), "Nd" (addr));
 }
 
-extern __inline__ void io_out16(const __kgi_u16_t val, const io_vaddr_t addr)
+extern __inline__ void 
+io_out16(const __kgi_u16_t val, const io_vaddr_t addr)
 {
+
 	__asm__ __volatile__ ("outw %w0,%w1" : : "a" (val), "Nd" (addr));
 }
 
-extern __inline__ void io_out32(const __kgi_u32_t val, io_vaddr_t addr)
+extern __inline__ void 
+io_out32(const __kgi_u32_t val, io_vaddr_t addr)
 {
+
 	__asm__ __volatile__ ("outl %0,%w1" : : "a" (val), "Nd" (addr));
 }
 
 
-extern __inline__ void io_ins8(const io_vaddr_t addr, void *buf, 
-	__kgi_size_t cnt)
+extern __inline__ void 
+io_ins8(const io_vaddr_t addr, void *buf, __kgi_size_t cnt)
 {
+
 	__asm__ __volatile__ ("cld ; rep ; insb" 
 		: "=D" (buf), "=c" (cnt) : "d" (addr), "0" (buf), "1" (cnt));
 }
 
-extern __inline__ void io_ins16(const io_vaddr_t addr, void * buf,
-	__kgi_size_t cnt)
+extern __inline__ void 
+io_ins16(const io_vaddr_t addr, void *buf, __kgi_size_t cnt)
 {
+
 	__asm__ __volatile__ ("cld ; rep ; insw" 
 		: "=D" (buf), "=c" (cnt) : "d" (addr), "0" (buf), "1" (cnt));
 }
 
-extern __inline__ void io_ins32(const io_vaddr_t addr, void *buf,
-	__kgi_size_t cnt)
+extern __inline__ void 
+io_ins32(const io_vaddr_t addr, void *buf, __kgi_size_t cnt)
 {
+
 	__asm__ __volatile__ ("cld ; rep ; insl"
 		: "=D" (buf), "=c" (cnt) : "d" (addr), "0" (buf), "1" (cnt));
 }
 
-extern __inline__ void io_outs8(const io_vaddr_t addr, const void *buf,
-	__kgi_size_t cnt)
+extern __inline__ void 
+io_outs8(const io_vaddr_t addr, const void *buf, __kgi_size_t cnt)
 {
+
 	__asm__ __volatile__ ("cld ; rep ; outsb"
 		: "=S" (buf), "=c" (cnt) : "d" (addr), "0" (buf), "1" (cnt));
 }
 
-extern __inline__ void io_outs16(const io_vaddr_t addr, const void *buf,
-	__kgi_size_t cnt)
+extern __inline__ void 
+io_outs16(const io_vaddr_t addr, const void *buf, __kgi_size_t cnt)
 {
+
 	__asm__ __volatile__ ("cld ; rep ; outsw"
 		: "=S" (buf), "=c" (cnt) : "d" (addr), "0" (buf), "1" (cnt));
 }
 
-extern __inline__ void io_outs32(const io_vaddr_t addr, const void *buf,
-	__kgi_size_t cnt)
+extern __inline__ void 
+io_outs32(const io_vaddr_t addr, const void *buf, __kgi_size_t cnt)
 {
+
 	__asm__ __volatile__ ("cld ; rep ; outsl"
 		: "=S" (buf), "=c" (cnt) : "d" (addr),"0" (buf), "1" (cnt));
 }
